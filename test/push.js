89d977b7981542acf8ea9a98e8a27d09614149cd
Uğur Baltacı <me@ugurbaltaci.com>
2014-11-14 15:42:18 +0200

    initial

--- test/push.js
+++ test/push.js
@@ -9,11 +9,11 @@ var expect = require("chai").expect;
 var credentials;
 var mechanicPush;
 
-describe("Mechanic Push Test", function () {
+describe("Mechanic Push:", function () {
 
     var provider = "PARSE";
 
-    describe("Not Valid Parameters", function () {
+    describe("Invalid Credentials:", function () {
         it("should fail with no credentials", function () {
             expect(function () {
                 new MechanicPush(provider, credentials);
@@ -26,10 +26,7 @@ describe("Mechanic Push Test", function () {
             }).to.throw(Error);
             expect(function () {
                 new MechanicPush(provider, {
-                    "parse": {
-                        "XX": "*******",
-                        "YY": "*******"
-                    }
+                    "UnknownProvider": {}
                 });
             }).to.throw(Error);
         });
@@ -38,16 +35,16 @@ describe("Mechanic Push Test", function () {
             expect(function () {
                 new MechanicPush("*****", {
                     "parse": {
-                        "XX": "*******",
-                        "YY": "*******"
+                        "NOTappId": "*******",
+                        "NOTrestApiKey": "*******"
                     }
                 });
             }).to.throw(Error);
         });
     });
 
-    describe("Valid Credentials", function () {
-        var msgToSend = "da";
+    describe("Valid Credentials:", function () {
+        var msgToSend = "MSG";
 
         before(function (done) {
             try {
@@ -60,11 +57,78 @@ describe("Mechanic Push Test", function () {
             }
         });
 
-        it("should do what for empty list", function (done) {
-            mechanicPush.sendBroadcast(msgToSend, Date.now() + 2*60*1001).then(function () {
+        it("Invalid Push Id should throw error", function (done) {
+            mechanicPush.sendBroadcast(1, msgToSend, Date.now() + 2*60*1001).then(function () {
+                done("Should not be executed");
+            }).catch(function (error) {
+                expect(error).to.exist();
+                done();
+            });
+        });
+
+        it("Past later date should throw error", function (done) {
+            mechanicPush.sendBroadcast("1", msgToSend, Date.now() - 2*60*1001).then(function () {
+                done("Should not be executed");
+            }).catch(function (error) {
+                expect(error).to.exist();
+                done();
+            });
+        });
+
+        it("Immediate later date should throw error", function (done) {
+            mechanicPush.sendBroadcast("1", msgToSend, Date.now() + 1001).then(function () {
+                done("Should not be executed");
+            }).catch(function (error) {
+                expect(error).to.exist();
+                done();
+            });
+        });
+
+        it("Far later date should throw error", function (done) {
+            mechanicPush.sendBroadcast("1", msgToSend, Date.now() + 14*24*60*60*1001).then(function () {
+                done("Should not be executed");
+            }).catch(function (error) {
+                expect(error).to.exist();
+                done();
+            });
+        });
+
+        it("Should send push immediately", function (done) {
+            mechanicPush.sendBroadcast("1", msgToSend).then(function () {
                 done();
             }).catch(function (error) {
-                console.log("-----" , error);
+                expect(error).to.not.exist();
+                done(error);
+            });
+        });
+
+        it("Should send push for 3 minutes later", function (done) {
+            mechanicPush.sendBroadcast("1", msgToSend, Date.now() + 3*60*1000).then(function () {
+                done();
+            }).catch(function (error) {
+                expect(error).to.not.exist();
+                done(error);
+            });
+        });
+
+        it("Should send listed tokens immediately", function (done) {
+            mechanicPush.sendPush("1", msgToSend + " individual token", ["9a3693623660837ea8fd75167288802b1697d4d33ffef8ec31452ad605217477"]).then(function () {
+                done();
+            }).catch(function (error) {
+                console.log(error);
+
+                expect(error).to.not.exist();
+                done(error);
+            });
+        });
+
+        it("Should send listed tokens 3 minutes later", function (done) {
+            mechanicPush.sendPush("1", msgToSend + " individual token", ["9a3693623660837ea8fd75167288802b1697d4d33ffef8ec31452ad605217477"], Date.now() + 3*60*1000).then(function () {
+                done();
+            }).catch(function (error) {
+                console.log(error);
+
+                expect(error).to.not.exist();
                 done(error);
             });
         });
